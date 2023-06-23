92c294103047eb021be9f5fd4371e653e1371783
Carter <carreejoh4637@gmail.com>
2023-06-22 21:26:53 -0600

    Good enough

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
-const notes = require('./public/assets/db/db.json');
+const notes = require('./db/db.json');
 const uuid = require('./public/assets/js/uuid');
 // const index = require('./public/assets/js/index.js');
 
-const PORT = 5500;
+const PORT = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,7 +23,6 @@ app.get('/notes', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
-    console.log("I'm reached yaya");
 });
 
 app.post('/api/notes', (req, res) => {
@@ -37,14 +36,14 @@ app.post('/api/notes', (req, res) => {
         res.json(newNote);
         console.log(newNote);
         
-        fs.readFile('./public/assets/db/db.json', 'utf8', (err, data) => {
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if(err) {
                 console.log(err);
             } else {
                 const parsedNotes = JSON.parse(data);
                 parsedNotes.push(newNote);
 
-                fs.writeFile('./public/assets/db/db.json', JSON.stringify(parsedNotes, null, 1), 
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 1), 
                 (writeErr) => writeErr ? console.error(writeErr) : console.info("successfully added note"));
             }
         });
